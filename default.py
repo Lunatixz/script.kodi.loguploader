a2619be90422ca8afba8c9c3893246998a389303
ronie <ronie@users.noreply.github.com>
2016-08-23 23:46:23 +0200

    add options for oldlog and crashlog

--- default.py
+++ default.py
@@ -12,7 +12,7 @@ ADDON        = xbmcaddon.Addon()
 ADDONID      = ADDON.getAddonInfo('id')
 ADDONNAME    = ADDON.getAddonInfo('name')
 ADDONVERSION = ADDON.getAddonInfo('version')
-CWD          = ADDON.getAddonInfo('path').decode("utf-8")
+CWD          = ADDON.getAddonInfo('path').decode('utf-8')
 LANGUAGE     = ADDON.getLocalizedString
 
 socket.setdefaulttimeout(5)
@@ -20,13 +20,14 @@ socket.setdefaulttimeout(5)
 URL      = 'https://paste.ubuntu.com/'
 LOGPATH  = xbmc.translatePath('special://logpath')
 LOGFILE  = os.path.join(LOGPATH, 'kodi.log')
+OLDLOG   = os.path.join(LOGPATH, 'kodi.old.log')
 REPLACES = (('//.+?:.+?@', '//USER:PASSWORD@'),('<user>.+?</user>', '<user>USER</user>'),('<pass>.+?</pass>', '<pass>PASSWORD</pass>'),)
 
 def log(txt):
     if isinstance (txt,str):
-        txt = txt.decode("utf-8")
+        txt = txt.decode('utf-8')
     message = u'%s: %s' % (ADDONID, txt)
-    xbmc.log(msg=message.encode("utf-8"), level=xbmc.LOGDEBUG)
+    xbmc.log(msg=message.encode('utf-8'), level=xbmc.LOGDEBUG)
 
 # Custom urlopener to set user-agent
 class pasteURLopener(FancyURLopener):
@@ -34,29 +35,82 @@ class pasteURLopener(FancyURLopener):
 
 class Main:
     def __init__(self):
-        succes, data = self.readLog()
-        if succes:
-            content = self.cleanLog(data)
-            succes, result = self.postLog(content)
+        self.getSettings()
+        files = self.getFiles()
+        for item in files:
+            filetype = item[0]
+            if filetype == 'log':
+                error = LANGUAGE(32011)
+                name = LANGUAGE(32031)
+            elif filetype == 'oldlog':
+                error = LANGUAGE(32012)
+                name = LANGUAGE(32032)
+            elif filetype == 'crashlog':
+                error = LANGUAGE(32013)
+                name = LANGUAGE(32033)
+            succes, data = self.readLog(item[1])
             if succes:
-                self.showResult(LANGUAGE(32005) % result)
+                content = self.cleanLog(data)
+                succes, result = self.postLog(content)
+                if succes:
+                    self.showResult(LANGUAGE(32005) % (name, result))
+                else:
+                    self.showResult('%s[CR]%s' % (error, result))
             else:
-                self.showResult(result)
-        else:
-            self.showResult(result)
+                self.showResult('%s[CR]%s' % (error, result))
 
-    def readLog(self):
+    def getSettings(self):
+        self.oldlog = ADDON.getSetting('oldlog') == 'true'
+        self.crashlog = ADDON.getSetting('crashlog') == 'true'
+
+    def getFiles(self):
+        logfiles = []
+        logfiles.append(['log', LOGFILE])
+        if self.oldlog:
+            if xbmcvfs.exists(OLDLOG):
+                logfiles.append(['oldlog', OLDLOG])
+            else:
+                self.showResult(LANGUAGE(32021))
+        if self.crashlog:
+            crashlog_path = ''
+            items = []
+            if xbmc.getCondVisibility('system.platform.osx'): # untested, need confirmation this is the correct path/filename
+                crashlog_path = os.path.join(os.path.expanduser('~'), 'Library/Logs/DiagnosticReports/')
+                filematch = 'Kodi'
+            elif xbmc.getCondVisibility('system.platform.ios'): # untested, need confirmation this is the correct path/filename
+                crashlog_path = '/var/mobile/Library/Logs/CrashReporter/'
+                filematch = 'Kodi'
+            elif xbmc.getCondVisibility('system.platform.linux'):
+                crashlog_path = os.path.expanduser('~') # not 100% accurate, crashlogs are created in the dir kodi was started from
+                filematch = 'kodi_crashlog'
+            elif xbmc.getCondVisibility('system.platform.windows'):
+                self.showResult(LANGUAGE(32023))
+            elif xbmc.getCondVisibility('system.platform.android'):
+                self.showResult(LANGUAGE(32024))
+            if crashlog_path and os.path.isdir(crashlog_path):
+                dirs, files = xbmcvfs.listdir(crashlog_path)
+                for item in files:
+                    if filematch in item and os.path.isfile(os.path.join(crashlog_path, item)):
+                        items.append(os.path.join(crashlog_path, item))
+                        items.sort(key=lambda f: os.path.getmtime(f))
+                        lastcrash = items[-1]
+                        logfiles.append(['crashlog', lastcrash])
+            if len(items) == 0:
+                self.showResult(LANGUAGE(32022))
+        return logfiles
+
+    def readLog(self, path):
         try:
-            lf = xbmcvfs.File(LOGFILE)
+            lf = xbmcvfs.File(path)
             content = lf.read()
             lf.close()
             if content:
                 return True, content
             else:
-                log('logfile is empty')
+                log('file is empty')
                 return False, LANGUAGE(32001)
         except:
-            log('unable to read logfile')
+            log('unable to read file')
             return False, LANGUAGE(32002)
 
     def cleanLog(self, content):
@@ -91,7 +145,7 @@ class Main:
         dialog = xbmcgui.Dialog()
         confirm = dialog.ok(ADDONNAME, message)
 
-if ( __name__ == "__main__" ):
+if ( __name__ == '__main__' ):
     log('script version %s started' % ADDONVERSION)
     Main()
 log('script version %s ended' % ADDONVERSION)
